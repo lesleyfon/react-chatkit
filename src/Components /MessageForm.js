2c238a20033767829180b99fab0c9d64e338687d
lesley <lesleybanadzem2017@gmail.com>
2019-11-29 19:26:07 -0800

    Write templatesd for MessageForm component

--- src/Components /MessageForm.js
+++ src/Components /MessageForm.js
@@ -1,11 +1,57 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Form, Alert, FormGroup, FormControl, Button } from 'react-bootstrap'
 
-function MessageForm() {
+function MessageForm({user, error, sending, activeRoom}) {
+    const [messageText, setMessageText] = React.useState('')
+
+    const handleChange = e => {
+        setMessageText(e.target.value)
+    }
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+    }
     return (
-        <div>
-            
-        </div>
+        <div class="message-form ld-over">
+            {user.length > 1 && <small class="text-muted" >{ user.username }</small> }
+            {/*    */}
+            <Form onSubmit={handleSubmit} className= {`ld-over ${sending && 'running'}`}>
+                <div class="ld ld-ring ld-spin"></div>
+                <Alert>{error}</Alert>
+                <FormGroup>
+                    <FormControl  
+                        id="message-input"
+                        type="text"
+                        name='messageText'
+                        value={messageText}
+                        onChange={handleChange}
+                        placeholder="Enter Message"
+                        autoComplete="off"
+                        required
+                    />
+                    <div class="clearfix">
+                        <Button type="submit" variant="primary" className="float-right">
+                        Send
+                        </Button>
+                    </div>
+                </FormGroup>
+            </Form>
+      </div>
     )
 }
+const mapStateToProps = state =>{
 
-export default MessageForm
+    return {
+        user: state.user,
+        sending: state.sending,
+        error: state.error,
+        activeRoom: state.activeRoom
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return{
+        getters: () => dispatch({type:'hasError'}),
+        setLoading: ()=> dispatch({type: 'loading'})
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(MessageForm)
--- src/Components /MessageList.js
+++ src/Components /MessageList.js
@@ -1,18 +1,46 @@
 import React from 'react'
 import ScrollToBottom from 'react-scroll-to-bottom';
+import {connect} from 'react-redux';
+import { css } from 'glamor'
 
-
-
-function MessageList() {
-    const defaultStyles = {
-        height: '600px',
-        width: '400px'
-    }
+function MessageList({messages}) {
+    const defaultStyles = css({
+        height: '100px',
+        width: '100%',
+        background:'red'
+    })
+    
     return (
-        <ScrollToBottom styles={defaultStyles}>
-            
+        <div class="message-list">
+        <h4>Messages</h4>
+        <hr/>
+        <ScrollToBottom  id="chat-messages" className={`message-group`}  style={defaultStyles} >
+            {messages.length > 1 ? messages.map((message, i)=><div className='message' key={i}>
+                <div className="clearfix">
+                    <h4 className="message-title">{ message.name }</h4>
+                    <small className="text-muted float-right">{ message.username }</small>
+                </div>
+                <p className="message-text">
+                    { message.text }
+                </p>
+                <div className="clearfix">
+                    <small className="text-muted float-right">{ message.date }</small>
+                </div>  
+            </div>) : <p>No Messages Yet</p>}
         </ScrollToBottom>
+        </div>
     )
 }
 
-export default MessageList
+const mapStateToProps = state =>{
+    return {
+        messages: state.messages
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return{
+        getters: () => dispatch({type:'hasError'}),
+        setLoading: ()=> dispatch({type: 'loading'})
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(MessageList)
