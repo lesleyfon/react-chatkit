bbd76cc86fd30feaef0bc09b8144c5bfcaa4a575
lesley <lesleybanadzem2017@gmail.com>
2019-11-29 18:08:31 -0800

    Created conditionals for displaying users

--- src/Components /UserList.js
+++ src/Components /UserList.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { ListGroup ,ListGroupItem, Badge } from 'react-bootstrap'
 
-function UserList() {
+function UserList({users, loading}) {
+    
+    const statusColor = (status) =>{
+         return status === 'online' ? 'success' : 'warning'
+    }
     return (
-        <div>
-            
-        </div>
+        <div className="user-list">
+        <h4>Members</h4>
+        <hr />
+        <ListGroup>
+            {(users.length > 0) ? 
+                users.map((user, i)=> <ListGroupItem key={i}>{user.name}
+                {user.presence && <Badge variant={statusColor(user.presence)}>{user.presence}</Badge>}
+                </ListGroupItem>) :
+                <p>No Users Yet</p>
+            }
+        </ListGroup>
+      </div>
     )
 }
 
-export default UserList
+const mapStateToProps = state =>{
+    console.log(state)
+    return {
+        loading: state.loading,
+        users: state.users
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return{
+        getters: () => dispatch({type:'hasError'}),
+        setLoading: ()=> dispatch({type: 'loading'})
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserList)
