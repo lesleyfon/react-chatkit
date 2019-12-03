2f8d97c3e471e2b4baa54a84b6a7169d097c6473
lesley <lesleybanadzem2017@gmail.com>
2019-12-02 17:56:36 -0800

    Wrote logic to setmemebers to store

--- src/chatkit.js
+++ src/chatkit.js
@@ -1,4 +1,5 @@
 import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
+import moment from 'moment';
 const INSTANCE_LOCATOR = process.env.REACT_APP_INSTANCE_LOCATOR;
 const TOKEN_URL = process.env.REACT_APP_TOKEN_URL;
 const MESSAGE_LIMIT = Number(process.env.REACT_APP_MESSAGE_LIMIT) || 10;
@@ -17,6 +18,20 @@ async function connectUser (userId){
 
     return currentUser;
 }
+function setmembers(){
+
+    return ( dispatch, getState) => {
+        const state = getState();
+
+        const memebers = activeRoom.users.map(user => ({
+            username: user.id,
+            name: user.name,
+            presence: user.presence.state
+        }))
+        dispatch({type: 'SET_USERS', payload: memebers})
+    }
+}
+
 
 export default {
     connectUser
